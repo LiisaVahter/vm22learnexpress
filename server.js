45d6945f2b6e5c117090a696a0c47400895228e7
Liisa Vahter <liisa.vahter@tptlive.ee>
2024-01-31 14:08:10 +0200

    add new

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const nunjucks = require('nunjucks');
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded( {extended:true}));
+
 nunjucks.configure('views', {
     autoescape: true,
     express: app
@@ -22,6 +24,16 @@ app.get('/page2', (req, res) => {
     console.log(req.query);
     res.render('form.njk', req.query);
   //   console.log('somebody visited');
+  });
+
+  app.get('/circle', (req, res) => {
+    console.log(req.query);
+    res.render('circle.njk', req.query);
+  //   console.log('somebody visited');
+  });
+
+  app.post('/circle', (req, res) => {
+     res.json(req.body)
   });
 
 app.listen(port, () => {
