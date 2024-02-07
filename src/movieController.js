09eea7c27e7ebdabe5b7cfd30814adf6aee4f62d
Liisa Vahter <liisa.vahter@tptlive.ee>
2024-02-07 15:07:31 +0200

    add new stuff

--- src/movieController.js
+++ src/movieController.js
@@ -3,10 +3,8 @@ const router = express.Router();
 const fs = require('fs');
 
 router.get('/' , (req,res) => {
-    //fs.appendFileSync('test.txt', 'hello');
-    let text = fs.readFileSync('test.txt', 'utf-8');
-    let movies = text.split('\n');
-    movies.splice(-1);
+    let movies = fs.readFileSync('movies.json', 'utf-8');
+    movies = JSON.parse(movies);
     res.render('movies/index.njk', {movies: movies});
 
 });
@@ -18,7 +16,15 @@ router.get('/add' , (req,res) => {
 });
 
 router.post('/add' , (req,res) => {
-   fs.appendFileSync('test.txt', req.body.movie + '\n');
+    let movies = fs.readFileSync('movies.json', 'utf-8');
+    movies = JSON.parse(movies);
+    movies.push({
+        name: req.body.movie,
+        year: req.body.year,
+        description: req.body.description,
+    });
+    let json = JSON.stringify(movies);
+   fs.writeFileSync('movies.json' , json);
    res.redirect('/movies/');
 
 });
